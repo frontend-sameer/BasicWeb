130143e6bb2f5b3696cfbcac4bda91a0ff58159a
user.email like:"frontend.sameer@gmail.com <user.email like:"frontend.sameer@gmail.com>
2025-02-01 13:55:25 +0530

    Service Completed

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -4,15 +4,22 @@ const Service = () => {
   return (
     <>
       <div className="w-full h-52 px-12">
-        <div className="w-full h-full bg-[#FFAA17]">
-          <div className="w-90 h-full bg-blue-300">
+        <div className="w-full h-full bg-[#FFAA17] flex items-center">
+          <div className="w-[45%] h-full">
             <img
               src="https://oitech-vite.vercel.app/assets/image-2-DfOzdorD.jpg"
               className="w-full h-full"
               alt=""
             />
           </div>
-          <h1>We're Ready Develop Your Site!</h1>
+          <div className="flex items-center w-full h-full justify-evenly">
+            <h1 className="text-[2.8vw] text-white font-bold w-80">
+              We're Ready Develop Your Site!
+            </h1>
+            <button className="bg-[#222429] font-semibold text-white px-6 py-4 rounded-sm cursor-pointer w-52 flex items-center justify-center">
+              More Services
+            </button>
+          </div>
         </div>
       </div>
     </>
